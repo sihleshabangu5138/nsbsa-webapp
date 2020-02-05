c7bf41ca9a108dfbff1247c8c22fcb9d709b205a
deepali52 <53506636+deepali52@users.noreply.github.com>
2020-02-05 13:59:28 +0530

    Update user.js

--- routes/adduser.js
+++ routes/adduser.js
@@ -54,10 +54,10 @@ router.route('/:id?')
 			result_data=result;
 			result_data[0].id_d=ObjectId(result_data[0].role).toString();
 		var query ={"user_id": ObjectId(id)};
-		var family_data=[];
-		dbo.collection("familydata").find(query).toArray(function(err, family_data1) {
-			family_data=family_data1;
-			family_data[0].id_d=ObjectId(family_data1[0]._id).toString();
+		// var family_data=[];
+		// dbo.collection("familydata").find(query).toArray(function(err, family_data1) {
+			// family_data=family_data1;
+			// family_data[0].id_d=ObjectId(family_data1[0]._id).toString();
 		dbo.collection("Role").find().toArray(function(err, role_name) {
 			fs.readFile('public/data/countries.json', function(err, data) { 
 			var jsonData = data;
